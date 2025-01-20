c50b4cf96fd5440f5200a5b91800a6dcb5dcd0de
Minkyung02 <minkyung22@swu.ac.kr>
2025-01-20 19:40:38 +0900

    Refactor: formatted change

--- src/dtos/challenge/list.dto.js
+++ src/dtos/challenge/list.dto.js
@@ -2,9 +2,9 @@ import { ChallengeStatus } from '@prisma/client';
 import moment from 'moment-timezone';
 
 const bodyToChallenge = body => {
-  const formattedJoinDate = moment.tz(body.joinDate, 'YYYY-MM-DD 00:00:00.000000', 'Asia/Seoul');
+  const formatted_JoinDate = moment.tz(body.joinDate, 'YYYY-MM-DD 00:00:00.000000', 'Asia/Seoul');
   console.log(formattedJoinDate);
-  const formattedEndDate = moment.tz(body.endDate, 'YYYY-MM-DD 00:00:00.000000', 'Asia/Seoul');
+  const formatted_EndDate = moment.tz(body.endDate, 'YYYY-MM-DD 00:00:00.000000', 'Asia/Seoul');
   return {
     name: body.name,
     owner_id: body.owner_id,
@@ -16,8 +16,8 @@ const bodyToChallenge = body => {
     maxParticipants: body.maxParticipants,
     verificationType: body.verificationType,
     rule: body.rule,
-    joinDate: formattedJoinDate,
-    endDate: formattedEndDate,
+    joinDate: formatted_JoinDate,
+    endDate: formatted_EndDate,
   };
 };
 

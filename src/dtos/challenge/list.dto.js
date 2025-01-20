c4ab89817f8abcb7d8e0178006cbe5064209887f
Minkyung02 <minkyung22@swu.ac.kr>
2025-01-20 17:54:19 +0900

    Feat: Add create challenge api

---
+++ src/dtos/challenge/list.dto.js
@@ -0,0 +1,25 @@
+import { ChallengeStatus } from '@prisma/client';
+import moment from 'moment-timezone';
+
+const bodyToChallenge = body => {
+  const formattedJoinDate = moment.tz(body.joinDate, 'YYYY-MM-DD 00:00:00.000000', 'Asia/Seoul');
+  console.log(formattedJoinDate);
+  const formattedEndDate = moment.tz(body.endDate, 'YYYY-MM-DD 00:00:00.000000', 'Asia/Seoul');
+  return {
+    name: body.name,
+    owner_id: body.owner_id,
+    type: body.type,
+    description: body.description,
+    challengeImage: body.challengeImage,
+    challengeStatus: body.challengeStatus,
+    maxParticipants: body.maxParticipants,
+    verificationType: body.verificationType,
+    rule: body.rule,
+    joinDate: formattedJoinDate,
+    endDate: formattedEndDate,
+  };
+};
+
+export default {
+  bodyToChallenge,
+};
---
+++ src/errors/challenge/list.error.js
@@ -0,0 +1,48 @@
+export class CreateChallengeError extends Error {
+  errorCode = 'B001';
+  constructor(reason, data) {
+    super(reason);
+    this.reason = reason;
+    this.statuscode = 400;
+    this.data = data;
+  }
+}
+
+export class SendChallengeError extends Error {
+  errorCode = 'B002';
+  constructor(reason, data) {
+    super(reason);
+    this.reason = reason;
+    this.statuscode = 400;
+    this.data = data;
+  }
+}
+
+//인증 실패 오류
+export class ChallengeExpiredError extends Error {
+  errorCode = 'B003';
+  constructor(reason, data) {
+    super(reason);
+    this.reason = reason;
+    this.statuscode = 400;
+    this.data = data;
+  }
+}
+
+//서버오류
+export class DataBaseError extends Error {
+  errorCode = 'B004';
+  constructor(reason, data) {
+    super(reason);
+    this.reason = reason;
+    this.statuscode = 400;
+    this.data = data;
+  }
+}
+
+export default {
+  CreateChallengeError,
+  DataBaseError,
+  ChallengeExpiredError,
+  SendChallengeError,
+};

7cf56b6fff994894160cb5cb2355e545dc3a7360
sehwanii_mac <sehwan1023@gmail.com>
2025-02-06 20:32:14 +0900

    Fix: Fix json return convention

--- src/dtos/verification/like.dto.js
+++ src/dtos/verification/like.dto.js
@@ -1,7 +1,7 @@
 const likeVerificationServiceToController = data => {
   const response_data = {
-    user_id: data[0].user_id,
-    verification_id: data[0].verification_id,
+    userId: data[0].user_id,
+    verificationId: data[0].verification_id,
     likesCount: data[1].likesCount,
   };
   return response_data;

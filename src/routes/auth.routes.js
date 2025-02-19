d58c76d76ef7c18929ecc5230a04819f092c69d7
sehwanii_mac <sehwan1023@gmail.com>
2025-02-19 13:37:17 +0900

    Feat: password   refresh logics

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import authController from '../controllers/auth.controller.js';
-
+import authMiddleware from '../middlewares/auth.middleware.js';
 const router = express.Router();
 
 // Login Route
@@ -9,14 +9,14 @@ router.post('/login/kakao', authController.kakaoLogin);
 router.post('/login/naver', authController.naverLogin);
 
 router.post('/find-email', authController.findEmail);
-router.post('/reset-password/phone', authController.ressetPasswordByPhone);
-router.post('/reset-password/email', authController.ressetPasswordByEmail);
+router.post('/reset-password/phone', authController.resetPasswordByPhone);
+router.post('/password/reset', authController.resetPasswordByEmail);
 
 router.post('/register', authController.register);
 router.post('/send-verify-email', authController.sendVerificationCode);
 router.post('/check-email-verification-code', authController.checkEmailVerificationCode);
 router.post('/check-nickname', authController.checkNickname);
-
+router.patch('/password', authMiddleware, authController.changePassword);
 // Refresh Token Route
 router.post('/token', authController.refreshToken);
 
